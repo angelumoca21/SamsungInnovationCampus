608a96ca1c700c5845d46da26ace866eab40a7e0
angelumoca21 <angelmoca21@gmail.com>
2022-11-02 12:18:36 -0600

    act

---
+++ modulos-js/expCalc.js
@@ -0,0 +1,2 @@
+var calc = require('./calc');
+console.log('After splitting into modules - ' + 'the result of calling calc.add function:%d',calc.add(10,10));
---
+++ modulos-js/suma.js
@@ -0,0 +1,5 @@
+var calc = {};
+calc.suma = function(a,b){
+    return a + b;
+}
+console.log('Before splitting into modules - ' + 'the result of calling calc.suma function: %d',calc.suma(10,10));
